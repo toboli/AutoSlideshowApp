4f080c9eeb59192a2989a334c2f915aa4b0677ee
渡部かすみ <kasumisheep@gmail.com>
2017-06-10 22:51:10 +0900

    変更

--- app/src/main/java/jp/techacademy/kasumi/watanabe/autoslideshowapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/kasumi/watanabe/autoslideshowapp/MainActivity.java
@@ -12,16 +12,25 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Button;
 import android.view.View;
+import java.util.Timer;
+import java.util.TimerTask;
+import android.os.Handler;
 
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final int PERMISSIONS_REQUEST_CODE = 100;
     private Cursor cursor = null; // MainActivity のプロパティ変数
+
+    /*タイマーの変数など*/
+    Timer mTimer;
+    double mTimerSec = 0.0;
+    Handler mHandler = new Handler();
+
+    ContentResolver resolver;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,13 +55,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //リスナー登録
         Button prev_button = (Button) findViewById(R.id.prev_button);
         prev_button.setOnClickListener(this);
+
         Button run_button = (Button) findViewById(R.id.run_button);
         run_button.setOnClickListener(this);
+
         Button next_button = (Button) findViewById(R.id.next_button);
         next_button.setOnClickListener(this);
 
-
     }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
@@ -69,47 +80,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void getContentsInfo() {
 
-        // 画像の情報を取得する
-        ContentResolver resolver = getContentResolver();
-        Cursor cursor = resolver.query(
+        resolver = getContentResolver();
+        cursor = resolver.query(
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
                 null, // 項目(null = 全項目)
                 null, // フィルタ条件(null = フィルタなし)
                 null, // フィルタ用パラメータ
                 null // ソート (null ソートなし)
+
         );
 
         if (cursor.moveToFirst()) {
-                // indexからIDを取得し、そのIDから画像のURIを取得する
-                int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-                Long id = cursor.getLong(fieldIndex);
-                Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+            // indexからIDを取得し、そのIDから画像のURIを取得する
+            int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+            Long id = cursor.getLong(fieldIndex);
+            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
 
-                Log.d("ANDROID", "URI : " + imageUri.toString());
+            Log.d("ANDROID", "URI : " + imageUri.toString());
 
             ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
             imageVIew.setImageURI(imageUri);
         }
-        cursor.close();
+        //cursor.close();
     }
+
+
 
     //戻るボタンを押したら、次の画像が表示される
     private void previous() {
-
-        if(cursor==null){
-            // 画像の情報を取得する
-            ContentResolver resolver = getContentResolver();
-            Cursor cursor = resolver.query(
+        if (cursor == null) {
+            resolver = getContentResolver();
+            cursor = resolver.query(
                     MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
                     null, // 項目(null = 全項目)
                     null, // フィルタ条件(null = フィルタなし)
                     null, // フィルタ用パラメータ
                     null // ソート (null ソートなし)
-                     );
+
+
+            );
 
         }
 
-        if (cursor.moveToPrevious()){
+        if (cursor.moveToPrevious()) {
             // indexからIDを取得し、そのIDから画像のURIを取得する
             int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
             Long id = cursor.getLong(fieldIndex);
@@ -121,10 +134,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             imageVIew.setImageURI(imageUri);
 
 
-        }
-
-        if(cursor.moveToPrevious() || cursor.moveToFirst())
-        {
+        } else if (cursor.moveToPrevious() || cursor.moveToLast()) {
             int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
             Long id = cursor.getLong(fieldIndex);
             Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
@@ -133,16 +143,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
             imageVIew.setImageURI(imageUri);
         }
-        cursor.close();
+        //cursor.close();
     }
 
     //進むボタンを押したら、一つ前の画像が表示される
     private void next() {
 
-        if(cursor==null){
-            // 画像の情報を取得する
-            ContentResolver resolver = getContentResolver();
-            Cursor cursor = resolver.query(
+        if (cursor == null) {
+            resolver = getContentResolver();
+            cursor = resolver.query(
                     MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
                     null, // 項目(null = 全項目)
                     null, // フィルタ条件(null = フィルタなし)
@@ -152,25 +161,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             );
 
-
         }
 
-        if (cursor.moveToNext()){
-                // indexからIDを取得し、そのIDから画像のURIを取得する
-                int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-                Long id = cursor.getLong(fieldIndex);
-                Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+        if (cursor.moveToNext()) {
+            // indexからIDを取得し、そのIDから画像のURIを取得する
+            int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+            Long id = cursor.getLong(fieldIndex);
+            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
 
-                Log.d("ANDROID", "URI : " + imageUri.toString());
+            Log.d("ANDROID", "URI : " + imageUri.toString());
 
             ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
             imageVIew.setImageURI(imageUri);
 
 
-        }
-
-        if(cursor.moveToNext() || cursor.moveToFirst())
-        {
+        } else if (cursor.moveToNext() || cursor.moveToFirst()) {
             int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
             Long id = cursor.getLong(fieldIndex);
             Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
@@ -179,17 +184,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
             imageVIew.setImageURI(imageUri);
         }
-        cursor.close();
+        //cursor.close();
     }
-
 
     @Override
     public void onClick(View v) {
+        /*戻るをおしたとき*/
         if(v.getId()==R.id.prev_button){
             previous();
+            /*進むをおしたとき*/
         }else if(v.getId()==R.id.next_button){
             next();
-        }
-    }
+            /*再生・停止をおしたとき*/
+        }else{
+            mTimer = new Timer();
+            mTimer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    mTimerSec += 0.1;
 
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                            Long id = cursor.getLong(fieldIndex);
+                            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+                            ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
+                            imageVIew.setImageURI(imageUri);
+
+                        }
+                    });
+                }
+            }, 100, 2000/*2秒ごとだから*/);
+
+        }
+
+    }
 }
